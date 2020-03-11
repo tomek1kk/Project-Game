852bf10edca3dcc2da1b86f5ee8bd7236f8cc36f
k1kli <karolklimaszewski0@gmail.com>
2020-03-11 11:59:03 +0100

    wszystkie pliki związane z asp.net umieszczone w jednym folderze

--- GameMaster/GameMaster/Program.cs
+++ GameMaster/GameMaster/Program.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -13,6 +15,7 @@ namespace GameMaster
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("aaaaaaaaaa " + Path.Combine( Directory.GetCurrentDirectory(), "aspnet"));
             CreateHostBuilder(args).Build().Run();
         }
 
--- GameMaster/GameMaster/aspnet/Startup.cs
+++ GameMaster/GameMaster/aspnet/Startup.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace GameMaster
@@ -23,6 +26,10 @@ namespace GameMaster
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.ViewLocationExpanders.Add(new MyViewLocationExpander());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,18 +43,33 @@ namespace GameMaster
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            app.UseStaticFiles();
-
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+            Path.Combine(Directory.GetCurrentDirectory(), "aspnet", "wwwroot"))
+            });
             app.UseRouting();
 
             app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+        public class MyViewLocationExpander : IViewLocationExpander
+        {
+            public void PopulateValues(ViewLocationExpanderContext context) { }
+
+            public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+            {
+                return new[]
+                {
+            "/aspnet/Views/{1}/{0}.cshtml",
+            "/aspnet/Views/Shared/{0}.cshtml"
+        }; // add `.Union(viewLocations)` to add default locations
+            }
         }
     }
 }
