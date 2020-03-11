573a4510ec501b8f0be84b1c501f81f38e8ac870
k1kli <karolklimaszewski0@gmail.com>
2020-03-11 10:43:51 +0100

    add game master initial files and modify CI config file to include building game master

---
+++ GameMaster/GameMaster/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameMaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
