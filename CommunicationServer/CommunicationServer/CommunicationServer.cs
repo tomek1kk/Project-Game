4047051a9b33540d993965d0de87f4c6fd20ec9e
tomek1kk <tomek1kkgw@gmail.com>
2020-03-13 11:29:55 +0100

    added communication server project, interface for message services

---
+++ CommunicationServer/CommunicationServer/CommunicationServer.cs
@@ -0,0 +1,24 @@
+﻿using CommunicationLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace CommunicationServer
+{
+    public class CommunicationServer
+    {
+        private Dictionary<int, Descriptor> correlation;
+        private Descriptor GMDescriptor;
+        
+        static void Main(string[] args)
+        {
+            // Get config
+
+        }
+
+        public void GetConfig()
+        {
+
+        }
+
+    }
+}
---
+++ CommunicationServer/CommunicationServer/CommunicationServerMessageService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using CommunicationLibrary;
+
+namespace CommunicationServer
+{
+    public class CommunicationServerMessageService : IMessageService
+    {
+        private TcpListener server;
+        private int gameMasterPort;
+        private int agentPort;
+        private int port;
+        private string ip;
+
+        public void InitService()
+        {
+            // GET CONFIG
+            IPAddress ipAddress = IPAddress.Parse(ip);
+
+            server = new TcpListener(ipAddress, port);
+            server.Start();
+        }
+
+        public void ListenForMessages()
+        {
+            TcpClient agent = new TcpClient("Agent", agentPort);
+            TcpClient gameMaster = new TcpClient("GameMaster", gameMasterPort);
+
+            while (true)
+            {
+
+            }
+        }
+
+        public bool Send<T>(T message, int port) where T : Message
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
