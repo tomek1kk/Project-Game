f178b63b14e1e0e9f267dca4e894b3d1b283248d
k1kli <karolklimaszewski0@gmail.com>
2020-03-16 15:41:13 +0100

    Reading message as string

---
+++ CommunicationLibrary/CommunicationLibrary/IDescriptor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunicationLibrary
+{
+    interface IDescriptor : IDisposable
+    {
+        void Send(Message m);
+        void SetReceiveCallback(Action<Message> receiveCallback);
+    }
+}
---
+++ CommunicationLibrary/CommunicationLibrary/MessageReceiving/RawMessageReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunicationLibrary.MessageReceiving
+{
+    public class RawMessageReader
+    {
+        public delegate int Read(byte[] buffer, int count);
+        private readonly Read _byteStreamReader;
+        private readonly byte[] _messageBuffer;
+        private readonly byte[] _messageLengthBuffer;
+
+        public RawMessageReader(Read byteStreamReader)
+        {
+            _byteStreamReader = byteStreamReader;
+            _messageBuffer = new byte[8 * 1024];
+            _messageLengthBuffer = new byte[2];
+        }
+        public string GetNextMessageAsString()
+        {
+            int bytesRead = _byteStreamReader(_messageLengthBuffer, 2);
+            if (bytesRead != 2) throw new Exception("Failed to read bytes");
+            if (!BitConverter.IsLittleEndian)
+                Array.Reverse(_messageLengthBuffer);
+            int messageLength = BitConverter.ToUInt16(_messageLengthBuffer, 0);
+            if (messageLength == 0) return String.Empty;
+            bytesRead = _byteStreamReader(_messageBuffer, messageLength);
+            if (bytesRead == 0) throw new Exception("Failed to read bytes");
+            return Encoding.UTF8.GetString(_messageBuffer, 0, bytesRead);
+        }
+    }
+}
