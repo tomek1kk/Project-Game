71de8a5ff8b47127c194d5f3b3c29952e3f8084d
k1kli <karolklimaszewski0@gmail.com>
2020-03-22 21:43:44 +0100

    added some tests to message handler

---
+++ Agent/Agent/Exceptions/AgentInfoNotValidException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agent.Exceptions
+{
+    public class AgentInfoNotValidException : Exception
+    {
+        public AgentInfoNotValidException() : base("AgentInfoNotValid") { }
+        public AgentInfoNotValidException(string message)
+            : base("Agent info not valid:" + message) { }
+        public AgentInfoNotValidException(string message, Exception inner)
+            : base("Agent info not valid:" + message, inner) { }
+    }
+}
