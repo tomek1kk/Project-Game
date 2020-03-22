bd5ee72859596dee9456f7ae5cb0bd2d9e547eeb
tomek1kk <tomek1kkgw@gmail.com>
2020-03-22 11:30:47 +0100

    gm request handling template'

--- GameMaster/GameMaster/MessageHandler.cs
+++ GameMaster/GameMaster/MessageHandler.cs
@@ -6,90 +6,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GameMaster.MessageHandlers;
 
 namespace GameMaster.Game
 {
-    public class MessageHandler
+    public abstract class MessageHandler
     {
-        public void HandleMessage(Message message, IMessageSenderReceiver communicator, Map map)
+        protected int agentId;
+
+        public void BaseReadMessage(Message message)
         {
-            if (message.GetPayload().ValidateMessage() == false || message.AgentId == null)
-            {
-                communicator.Send(new Message<NotDefinedError>()
-                {
-                    AgentId = message.AgentId,
-                    MessagePayload = new NotDefinedError()
-                });
-                return;
-            }
-
-            Message response = GetResponseMessage(message, map);
-            communicator.Send(response);
+            agentId = (int)message.AgentId;
+            ReadMessage(message.GetPayload());
         }
-
-        private Message GetResponseMessage(Message request, Map map)
+        public Message ProcessRequest(Map map)
         {
-            int agentId = (int)request.AgentId;
-            Message responseMessage = null;
-            Player agent;
-
-            switch (request.MessageId)
-            {
-                case MessageType.JoinGameRequest:
-                    JoinGameRequest joinGameRequest = (JoinGameRequest)request.GetPayload();
-                    JoinGameResponse response = map.AddPlayer(joinGameRequest.TeamId.ToTeam(), agentId);
-                    responseMessage = new Message<JoinGameResponse>()
-                    {
-                        AgentId = agentId,
-                        MessagePayload = response
-                    };
-                    break;
-                case MessageType.CheckHoldedPieceRequest:
-                    agent = map.GetPlayerById(agentId);
-                    responseMessage = new Message<CheckHoldedPieceResponse>()
-                    {
-                        AgentId = agentId,
-                        MessagePayload = agent.CheckHolding()
-                    };
-                    break;
-                case MessageType.DestroyPieceRequest:
-                    agent = map.GetPlayerById(agentId);
-                    responseMessage = new Message<DestroyPieceResponse>()
-                    {
-                        AgentId = agentId,
-                        MessagePayload = agent.DestroyHolding()
-                    };
-                    break;
-                case MessageType.DiscoveryRequest:
-                    responseMessage = new Message<DiscoveryResponse>()
-                    {
-                        AgentId = agentId,
-                        MessagePayload = map.Discovery(agentId)
-                    };
-                    break;
-                case MessageType.ExchangeInformationRequest:
-                    // TODO
-                    break;
-                case MessageType.MoveRequest:
-                    MoveRequest moveRequest = (MoveRequest)request.GetPayload();
-                    MoveResponse moveResponse = map.Move(moveRequest.Direction, agentId);
-                    responseMessage = new Message<MoveResponse>()
-                    {
-                        AgentId = agentId,
-                        MessagePayload = moveResponse
-                    };
-                    break;
-                case MessageType.PickPieceRequest:
-                    break;
-                case MessageType.PutPieceRequest:
-                    break;
-                case MessageType.RedirectedExchangeInformationRequest:
-                    break;
-                default:
-                    break;
-            }
-
-            return responseMessage;
+            if (CheckRequest(map))
+                Execute(map);
+            return GetResponse(map);
         }
+        public void SetTimeout()
+        {
+            // TODO
+            return;
+        }
+        protected abstract bool CheckRequest(Map map);
+        protected abstract Message GetResponse(Map map);
+        protected abstract void Execute(Map map);
+        protected abstract void ReadMessage(MessagePayload payload);
     }
 }
