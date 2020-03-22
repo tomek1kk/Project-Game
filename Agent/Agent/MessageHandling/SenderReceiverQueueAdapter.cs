b056edfd4592c5a44e0b8221713324af959f14a9
k1kli <karolklimaszewski0@gmail.com>
2020-03-22 19:48:52 +0100

    adapter for sender receiver that has built in blocking queue

---
+++ Agent/Agent/MessageHandling/SenderReceiverQueueAdapter.cs
@@ -0,0 +1,39 @@
+﻿using CommunicationLibrary;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Agent.MessageHandling
+{
+  
+    public class SenderReceiverQueueAdapter : IDisposable
+    {
+        private BlockingCollection<Message> _queue = new BlockingCollection<Message>();
+        private IMessageSenderReceiver _adaptedSenderReceiver;
+        public SenderReceiverQueueAdapter(IMessageSenderReceiver adaptedSenderReceiver)
+        {
+            _adaptedSenderReceiver = adaptedSenderReceiver;
+            _adaptedSenderReceiver.StartReceiving(message => _queue.Add(message));
+        }
+        public Message Take()
+        {
+            return _queue.Take();
+        }
+
+        public Message Take(CancellationToken cancellationToken)
+        {
+            return _queue.Take(cancellationToken);
+        }
+        public void Send(Message message)
+        {
+            _adaptedSenderReceiver.Send(message);
+        }
+
+        public void Dispose()
+        {
+            _adaptedSenderReceiver.Dispose();
+        }
+    }
+}
