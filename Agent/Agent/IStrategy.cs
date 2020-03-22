2925da529f727c62d7d67740ad900c6ec36d17ae
Polina Grinko <grinkopolina@gmail.com>
2020-03-22 14:26:31 +0100

    Moved board from AgentInfo to Strategy

--- Agent/Agent/IStrategy.cs
+++ Agent/Agent/IStrategy.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using Agent.AgentBoard;
+
 namespace Agent
 {
     public interface IStrategy
     {
+        Field[,] Board {get; set;}
         void MakeDecision(AgentInfo agent);
+        void UpdateMap(Message message);
 
     }
     public class SampleStrategy : IStrategy
-    { 
+    {
         public void MakeDecision(AgentInfo agent)
+        {
+
+        }
+        void UpdateMap(Message message)
         {
 
         }
