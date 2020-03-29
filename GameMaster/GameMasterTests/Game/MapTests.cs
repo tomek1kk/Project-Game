80215d06faa489e2ba9fec2638436bf36ff0f2f1
KicunKrzysztof <kicunkrzysztof@gmail.com>
2020-03-29 16:39:25 +0200

    created folder for messagehandlers tests

--- GameMaster/GameMasterTests/Game/MapTests.cs
+++ GameMaster/GameMasterTests/Game/MapTests.cs
@@ -35,59 +35,5 @@ namespace GameMasterTests.Game.Tests
             for (int i = 0; i < randomCount; i++)
                Assert.AreEqual(properResult[i], returnedList[i]);
         }
-        [TestMethod()]
-        public void TestMoveHandlerOutsideN()
-        {
-            //given
-            var players = new List<(int x, int y, int id, Team team)>() { (0, 0, 1, Team.Red) };
-            var map = new Map(players: players);
-            var message = new Message<MoveRequest>()
-            {
-                AgentId = 1,
-                MessagePayload = new MoveRequest()
-                {
-                    Direction = "N"
-                }
-            };
-            var moveHandler = new MoveRequestHandler();
-            //GMConfiguration config = GMConfiguration.ReadConfiguration(new string[0]);
-            GMConfiguration config = new GMConfiguration()
-            {
-                BoardX = 40,
-                BoardY = 40,
-                CsIP = "127.0.0.1",
-                CsPort = 8080,
-                TeamID = 3,
-                MovePenalty = 1500,
-                AskPenalty = 1000,
-                DiscoveryPenalty = 700,
-                PutPenalty = 500,
-                CheckForShamPenalty = 700,
-                ResponsePenalty = 1000,
-                GoalAreaHight = 5,
-                NumberOfGoals = 5,
-                NumberOfPieces = 10,
-                ShamPieceProbability = 20
-            };
-            Message<MoveError> expectedResult = new Message<MoveError>
-            {
-                AgentId = 1,
-                MessagePayload = new MoveError()
-                {
-                    Position = new Position()
-                    {
-                        X = 0,
-                        Y = 0
-                    }
-                }
-            };
-            //when
-            Message response = moveHandler.ProcessRequest(map, message, config);
-
-            //then
-            Assert.IsInstanceOfType(response, expectedResult.GetType());
-            Assert.AreEqual(expectedResult.MessagePayload.Position.X, ((MoveError)response.GetPayload()).Position.X);
-            Assert.AreEqual(expectedResult.MessagePayload.Position.Y, ((MoveError)response.GetPayload()).Position.Y);
-        }
     }
 }
