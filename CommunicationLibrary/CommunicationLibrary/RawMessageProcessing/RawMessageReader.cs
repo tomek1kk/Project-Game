fa886c076070cf41bf772da6f421c24ab7bfc92f
k1kli <karolklimaszewski0@gmail.com>
2020-05-20 23:09:59 +0200

    fixed message length reading

--- CommunicationLibrary/CommunicationLibrary/RawMessageProcessing/RawMessageReader.cs
+++ CommunicationLibrary/CommunicationLibrary/RawMessageProcessing/RawMessageReader.cs
@@ -19,18 +19,24 @@ namespace CommunicationLibrary.RawMessageProcessing
         }
         public string GetNextMessageAsString()
         {
-            int bytesRead = _byteStreamReader(_messageLengthBuffer, 4, 0);
-            if (bytesRead != 4) throw new Exception("Failed to read bytes");
+            int allBytesRead = 0;
+            while (allBytesRead != 4)
+            {
+                int bytesRead = _byteStreamReader(_messageLengthBuffer, 4 - allBytesRead, allBytesRead);
+                if (bytesRead == 0) throw new Exception("Failed to read len bytes");
+                allBytesRead += bytesRead;
+            }
             if (!BitConverter.IsLittleEndian)
                 Array.Reverse(_messageLengthBuffer);
             int messageLength = BitConverter.ToInt32(_messageLengthBuffer, 0);
             if (messageLength == 0) return String.Empty;
             Console.WriteLine($"Before reading, len={messageLength}");
-            int allBytesRead = 0;
+            allBytesRead = 0;
             while(allBytesRead != messageLength)
             {
-                allBytesRead += _byteStreamReader(_messageBuffer, messageLength-allBytesRead, allBytesRead);
-                if (allBytesRead == 0) throw new Exception("Failed to read bytes");
+                int bytesRead = _byteStreamReader(_messageBuffer, messageLength-allBytesRead, allBytesRead);
+                if (bytesRead == 0) throw new Exception("Failed to read mes bytes");
+                allBytesRead += bytesRead;
             }
             Console.WriteLine($"After reading, len={allBytesRead}");
             return Encoding.UTF8.GetString(_messageBuffer, 0, messageLength);
